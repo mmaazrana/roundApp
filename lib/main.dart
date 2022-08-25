7c4efd2977d31a456789eaf98870ed6b6dd65555
Maaz Rana <maazrana@192.168.109.128>
2022-08-25 18:50:17 +0500

    completed ios build

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textTheme: GoogleFonts.latoTextTheme(),
         primarySwatch: Colors.grey,
